4e45fa237c077d8fb398fbfbbb1195cfc18e8810
Midrooms <jajamako46@gmail.com>
2023-04-05 21:25:11 -0400

    CrepePlus 1.8

--- CrepePlus.bat
+++ CrepePlus.bat
@@ -23,18 +23,17 @@ goto menu
 start cmd /k "cd %cd%\CrepePackage\py && @echo off && title CrepePlus mitmdump && timeout /t 3 && mitmdump -s proxy.py -k"
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d http://127.0.0.1:8080 /f
-cd %cd%\CrepePackage
+cd..
 cls
 echo CrepePlus has enabled the proxy! Some internet functionality may not work.
 echo To disable the proxy, head to system settings - Network and internet -> Proxy -> Manual proxy -> Set up -> Use a proxy server checked off -> Save
 echo If you can't figure how to disable the proxy, simply restart your PC.
-
 npm run start
 
 :update
 @echo Updating CrepePlus...
 @RD /S /Q "%cd%\CrepePackage"
-powershell -Command "Invoke-WebRequest https://github.com/CrepePlus/CrepePackages/raw/main/CrepePackage.zip -Outfile CrepePackage.zip"
+powershell -Command "Invoke-WebRequest https://github.com/CrepePlus/CrepePackages/raw/main/CrepePackage.zip"
 powershell -Command "Expand-Archive -Path CrepePackage.zip CrepePackage -Force"
 DEL CrepePackage.zip
 start cmd /k "cd %cd%/CrepePackage && npm install"
