a627843ee1967dbdfdb7a64c78b993d513feb2ac
Midrooms <108638658+Midrooms@users.noreply.github.com>
2024-01-25 18:05:03 -0500

    Update CrepePlus.py

--- CrepePackage/PlusResources/CrepePlus.py
+++ CrepePackage/PlusResources/CrepePlus.py
@@ -8,8 +8,6 @@ rpc.connect()
 rpc.update(large_image="plus", details="Private server manager for SR!", state="In launcher")
 
 
-
-
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")
 
@@ -17,7 +15,6 @@ app = customtkinter.CTk()
 app.geometry("325x160")
 app.configure(bg="#242424")
 app.resizable(False, False)
-
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 app.wm_iconbitmap(os.path.join(current_dir, 'plus.ico'))
@@ -35,11 +32,9 @@ def button_callback1():
 def button_callback2():
     subprocess.run(f'cmd /c start cmd /k "cd /d {scriptsDir} && build.bat"', shell=True)
 
-
 def button_callback3():
     subprocess.run(f'cmd /c start cmd /k "cd /d {proxyDir} && stopProxy.bat"', shell=True)
     app.destroy()
-
 
 button1 = customtkinter.CTkButton(app, text="Start server & enable proxy", command=button_callback1, fg_color="#C6829B", hover_color="#843E58")
 button1.pack(pady=5)
