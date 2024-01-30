d7d8c8790cb0e3f21707731e1f2b0ccdbb608c16
Midrooms <108638658+Midrooms@users.noreply.github.com>
2024-01-30 02:09:26 -0500

    fix resources

---
+++ Resources/Astral/scripts.bat
@@ -0,0 +1,43 @@
+@echo off
+IF "%~1"=="1" goto 1
+IF "%~1"=="2" goto 2
+IF "%~1"=="3" goto 3
+goto end
+
+
+:1
+@echo off
+cd ..\LunarCore
+start cmd /k "java -jar LunarCore.jar"
+cd ..\Astral\proxy
+call StarRail.HttpProxy.exe
+exit
+goto end
+
+
+:2
+cd..
+cd %CD%\LunarCore
+gradlew jar
+pause
+exit
+goto end
+
+
+:3
+@echo off
+cd /d %~dp0
+taskkill /f /im StarRail.HttpProxy.exe
+certutil -user -delstore root ae8fe14dc72938ad5b1ed3889dd8e2ec619a50cf
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /d "" /f
+cls
+color 3
+echo The proxy has been disabled.
+echo You have to close the server terminal manually.
+pause
+exit
+goto end
+
+
+:end
