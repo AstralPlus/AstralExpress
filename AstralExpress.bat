b1c683ec2c78cb75ec5554beeb83e9112e770e8c
Midrooms <108638658+Midrooms@users.noreply.github.com>
2024-02-09 06:49:20 -0500

    Improved bat launcher

--- AstralExpress.bat
+++ AstralExpress.bat
@@ -1,6 +1,23 @@
 @echo off
 cd %CD%\Resources\Astral
-pip install customtkinter
-pip install pypresence
+
+python -c "import pkg_resources" 2> nul
+if errorlevel 1 (
+    echo "Python is not installed. Please install Python first."
+    exit /b
+)
+
+python -c "import customtkinter" 2> nul
+if errorlevel 1 (
+    echo "Installing customtkinter..."
+    pip install customtkinter
+)
+
+python -c "import pypresence" 2> nul
+if errorlevel 1 (
+    echo "Installing pypresence..."
+    pip install pypresence
+)
+
 start /min cmd /c "python AstralExpress.py"
 exit
